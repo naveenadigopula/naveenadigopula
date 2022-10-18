b5d2f4d4049952d5605408f023836ea3267790cb
naveenadigopula <111571633+naveenadigopula@users.noreply.github.com>
2022-10-18 12:20:55 +0530

    Add files via upload

--- jenkins-demo/src/main/java/com/naveen/jd/Calculator.java
+++ jenkins-demo/src/main/java/com/naveen/jd/Calculator.java
@@ -1,5 +1,26 @@
 package com.naveen.jd;
 
 public class Calculator {
+	 public int sum(int...x)
+	    {
+	        int s=0;
+	        for(int i:x)
+	            s=s+i;
+	        return s;
+	        
+	    }
+	    public int square(int x)
+	    {
+	        return x;
+	    }
+	    public int power(int x,int n)
+	    {
+	        int res = 1;
+	        for(;n>0;n--)
+	            res = res*x;
+	        return res;
+	    }
 
-}
+
+
+	}
--- jenkins-demo/src/test/java/com/naveen/jd/CalculatorTest.java
+++ jenkins-demo/src/test/java/com/naveen/jd/CalculatorTest.java
@@ -1,5 +1,39 @@
 package com.naveen.jd;
 
+import static org.junit.Assert.assertEquals;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
 public class CalculatorTest {
+    Calculator c;
+    @Before
+    public void setup() throws Exception{
+        
+    }
+    @After
+    public void tearDown() throws Exception{
+    c=null;
+    }
+    @Test
+    public void tearSum() {
+        assertEquals(100,c.sum(215,c.square(15)));
+    
+    }
+    
+    @Test
+    public void tearPower() {
+        assertEquals(125,c.sum(5,3));
+    
+    }
+    
+    
+
+
 
 }
